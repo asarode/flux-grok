c94faa3c7ab350734d95ec44a796cc121cdbd86d
arjun <sarodearjun57@gmail.com>
2015-07-23 00:19:50 -0400

    add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,27 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+// var watchify = require('watchify');
+var browserSync = require('browser-sync');
+// var gutil = require('gulp-utils')
+
+// var bundler = watchify(browserify('./public/scripts/main.js', watchify.args));
+// bundler.transform(babelify);
+// bundler.on('update', bundle);
+
+// function bundle() {
+//     gutil.log('Compiling scripts');
+
+//     return bundler.bundle()
+//         .on('error', function(err) {
+//             gutil.log(err.message);
+//             browserSync.notify('Error building scripts!');
+//             this.emit('end');
+//         })
+//         .pipe(source('bundle.js'))
+//         .pipe(gulp.dest('./dist/scripts'))
+//         .pipe()browserSync.reload
+// }
 
 gulp.task('browserify', function() {
     browserify('./public/scripts/main.js')
@@ -10,7 +31,8 @@ gulp.task('browserify', function() {
       }))
       .bundle()
       .pipe(source('bundle.js'))
-      .pipe(gulp.dest('dist/scripts'));
+      .pipe(gulp.dest('dist/scripts'))
+      .pipe(browserSync.reload({ stream: true, once: true }));
 });
 
 gulp.task('copy',function() {
@@ -22,6 +44,15 @@ gulp.task('copy',function() {
       .pipe(gulp.dest('dist/assets'));
 });
 
-gulp.task('default',['browserify', 'copy'], function() {
-    return gulp.watch('public/src/**/*.*', ['browserify', 'copy'])
+gulp.task('browserSync', function() {
+    browserSync({
+        server: './dist'
+    });
 });
+
+gulp.task('watch', function() {
+    gulp.watch('./public/scripts/**/*.*', ['browserify']);
+    gulp.watch('./public/index.html', ['copy']);
+});
+
+gulp.task('default',['watch', 'browserify', 'copy', 'browserSync']);
